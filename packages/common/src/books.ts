4b325db45bb627293e342233941c026884a9e011
823cccc <marcodsl@tuta.io>
2021-06-10 21:18:53 -0300

    chore: bootstrap projects

---
+++ packages/common/src/books.ts
@@ -0,0 +1,8 @@
+type Book = {
+  id: string
+  name: string
+  author: string
+  description: string
+}
+
+export { Book }
---
+++ packages/web/webpack.config.ts
@@ -0,0 +1,55 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import path from 'path'
+import webpack from 'webpack'
+
+const config: webpack.Configuration = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+              '@babel/preset-typescript',
+            ],
+          },
+        },
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+        type: 'asset/inline',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'build', 'index.html'),
+    }),
+  ],
+}
+
+const devServer = {
+  port: 3000,
+  compress: true,
+  contentBase: path.resolve(__dirname, 'dist'),
+  historyApiFallback: true,
+}
+
+export default { ...config, devServer }
