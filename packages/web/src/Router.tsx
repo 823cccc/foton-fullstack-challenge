5189d79f08472e1729f2117be4b53d467e8e96f1
823cccc <marcodsl@tuta.io>
2021-06-17 21:28:57 -0300

    web: attempt to fix router with gh pages

--- packages/web/src/Router.tsx
+++ packages/web/src/Router.tsx
@@ -13,28 +13,26 @@ import {
   Search,
 } from './pages'
 
-const Router = (): JSX.Element => {
-  return (
-    <HashRouter basename="/">
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/404" component={NotFound} />
+const Router = (): JSX.Element => (
+  <HashRouter basename="/">
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/404" component={NotFound} />
 
-        <Route exact path="/search" component={Search} />
-        <Route path="/search/:query" component={Search} />
+      <Route exact path="/search" component={Search} />
+      <Route exact path="/search/:query" component={Search} />
 
-        <Route exact path="/profile" component={Profile} />
-        <Route exact path="/register" component={Register} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/logout" component={Logout} />
+      <Route exact path="/profile" component={Profile} />
+      <Route exact path="/register" component={Register} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/logout" component={Logout} />
 
-        <Route exact path="/books/new" component={AddBook} />
-        <Route path="/books/:id" component={Details} />
+      <Route exact path="/books/new" component={AddBook} />
+      <Route exact path="/books/:id" component={Details} />
 
-        <Route path="*" component={NotFound} />
-      </Switch>
-    </HashRouter>
-  )
-}
+      <Route path="*" component={NotFound} />
+    </Switch>
+  </HashRouter>
+)
 
 export { Router }
